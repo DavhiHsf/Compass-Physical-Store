7db22e9ad2e4c302848bf108095731b79355c432
Davi Henrique <henridavhi26@gmail.com>
2025-04-21 21:06:09 -0300

    feat: implementação da classe CepDataResponseDto e atualização do controlador e serviço ViaCep para validação e consulta de Cep

--- src/integrations/viacep/viacep.service.ts
+++ src/integrations/viacep/viacep.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, BadRequestException, NotFoundException } from '@nestjs/common';
 import axios from 'axios';
 
 @Injectable()
@@ -6,11 +6,13 @@ export class ViaCepService {
   private readonly logger = new Logger(ViaCepService.name);
 
   private validateCepFormat(cep: string): void {
-    const isValid = /^\d{5}-\d{3}$/.test(cep) || /^\d{8}$/.test(cep);
-    if (!isValid) {
-      this.logger.warn(`CEP inválido inserido: ${cep}. Use o formato XXXXX-XXX ou XXXXXXXX.`);
-      throw new Error('CEP inválido!');
+    const cepValid = /^\d{5}-\d{3}$/.test(cep) || /^\d{8}$/.test(cep);
+    if (!cepValid) {
+      this.logger.warn(`Cep inválido: ${cep}. O formato correto é 00000-000 ou 00000000.`);
+        throw new BadRequestException("Cep inválido!");
     }
+    this.logger.log(`Formato do CEP ${cep} validado com sucesso.`);
+
   }
 
   async rescueCepData(cep: string): Promise<any> {
@@ -21,14 +23,23 @@ export class ViaCepService {
 
       if (!response.data || response.data.erro) {
         this.logger.warn(`O Cep ${cep} não foi localizado.`);
-        throw new Error('Cep não localizado!');
+        throw new NotFoundException('Cep não localizado!');
       }
+
+      this.logger.log(`Buscando dados para o Cep: ${cep}`);
+
+      this.logger.log(`Dados do Cep ${cep} localizados com sucesso.`);
 
       return response.data;
       
     } catch (error) {
       this.logger.error(`Erro ao consultar o Cep ${cep}: ${error.message}`);
-      throw new Error('Erro ao localizar o Cep!');
+
+      if (error instanceof BadRequestException || error instanceof NotFoundException) {
+      throw error;
+      }
+
+      throw new Error('Erro interno ao buscar o CEP.');
     }
   }
 }
